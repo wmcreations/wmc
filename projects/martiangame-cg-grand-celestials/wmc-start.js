4076f3911e8d97a57bf31ec2056d1e9e8297f132
wmcreations <wmcreations@outlook.com>
2016-03-06 18:09:39 +0800

    [WMC] Updates - moved repo to new folder named 'projects'

---
+++ projects/martiangame-cg-grand-celestials/wmc-start.js
@@ -0,0 +1,17 @@
+(function(GameAsync, undefined){
+  
+  // Checks existing functions and execute
+  var storageFns = ['login','start','server'];
+  for (var fnInput in window){
+    // console.debug(fnInput);
+    for(var i = 0; i < storageFns.length; i++){
+      if(fnInput === storageFns[i]){
+        console.debug('WMC Functions >>>>', fnInput);
+      }
+      // if(i === storageFns.length){
+      //   return;
+      // }
+    }    
+  }
+  
+}(this, undefined));
