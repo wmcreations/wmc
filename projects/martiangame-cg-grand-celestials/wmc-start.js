8d50a7380b70ff8805522baf090959ee01c5f886
wmcreations <wmcreations@outlook.com>
2016-04-02 22:50:18 +0800

    [SDK] & [Database] Updates on SDK - Implemented easy html templating and added configure settings for easy adjustments for sdk.

--- projects/martiangame-cg-grand-celestials/wmc-start.js
+++ projects/martiangame-cg-grand-celestials/wmc-start.js
@@ -1,17 +1,17 @@
-(function(GameAsync, undefined){
+// (function(GameAsync, undefined){
   
-  // Checks existing functions and execute
-  var storageFns = ['login','start','server'];
-  for (var fnInput in window){
-    // console.debug(fnInput);
-    for(var i = 0; i < storageFns.length; i++){
-      if(fnInput === storageFns[i]){
-        console.debug('WMC Functions >>>>', fnInput);
-      }
-      // if(i === storageFns.length){
-      //   return;
-      // }
-    }    
-  }
+//   // Checks existing functions and execute
+//   var storageFns = ['login','start','server'];
+//   for (var fnInput in window){
+//     // console.debug(fnInput);
+//     for(var i = 0; i < storageFns.length; i++){
+//       if(fnInput === storageFns[i]){
+//         console.debug('WMC Functions >>>>', fnInput);
+//       }
+//       // if(i === storageFns.length){
+//       //   return;
+//       // }
+//     }    
+//   }
   
-}(this, undefined));
+// }(this, undefined));
